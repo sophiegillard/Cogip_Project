68d886491a01b5f181b2c08789c7f3b178c15075
Sophie Gillard <sophie.x.gillard@gmail.com>
2022-09-20 10:52:15 +0200

    fail try

--- Resources/views/table.php
+++ Resources/views/table.php
@@ -1,5 +1,15 @@
-<?php
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../../public/assets/css/reset.css" rel="stylesheet" type="text/css">
+    <link href="../../public/assets/css/styles.css" rel="stylesheet" type="text/css">
+    <title>Cogip - table</title>
+</head>
+<body>
+
 <h1>table</h1>
 
 <div class="table-container" role="table" aria-label="Destinations">
@@ -54,3 +64,5 @@
         <div class="flex-row">US$400</div>
     </div>
 </div>
+
+</body>
