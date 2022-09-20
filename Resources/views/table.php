8fde7f11d74dc9939476084d9dd393500f097fa8
Sophie Gillard <sophie.x.gillard@gmail.com>
2022-09-20 09:44:54 +0200

    creation first draft table

---
+++ Resources/views/table.php
@@ -0,0 +1,56 @@
+<?php
+?>
+<h1>table</h1>
+
+<div class="table-container" role="table" aria-label="Destinations">
+    <div class="flex-table header" role="rowgroup">
+        <div class="flex-row first" role="columnheader">Country</div>
+        <div class="flex-row" role="columnheader">Events</div>
+        <div class="flex-row" role="columnheader">Time</div>
+        <div class="flex-row" role="columnheader">Fees</div>
+    </div>
+    <div class="flex-table row" role="rowgroup">
+        <div class="flex-row first" role="cell"><span class="flag-icon flag-icon-gb"></span> United Kingdom</div>
+        <div class="flex-row" role="cell">Stonehenge, Windsor and Bath with Pub Lunch </div>
+        <div class="flex-row" role="cell">19 Sep, 1p.m.</div>
+        <div class="flex-row" role="cell">US$500</div>
+    </div>
+    <div class="flex-table row" role="rowgroup">
+        <div class="flex-row first"  role="cell"><span class="flag-icon flag-icon-ca"></span> Canada</div>
+        <div class="flex-row" role="cell">Vancouver to Victoria and Butchart Gardens Tour </div>
+        <div class="flex-row" role="cell">23 Sep, 1:30p.m.</div>
+        <div class="flex-row" role="cell">US$387</div>
+    </div>
+    <div class="flex-table row" role="rowgroup">
+        <div class="flex-row rowspan first"><span class="flag-icon flag-icon-us"></span> United States</div>
+        <div class="column">
+            <div class="flex-row">
+                <div class="flex-cell" role="cell">Napa and Sonoma Wine Country Tour</div>
+                <div class="flex-cell" role="cell">5 Sep, 9p.m.</div>
+                <div class="flex-cell" role="cell">US$600</div>
+            </div>
+            <div class="flex-row" role="rowgroup">
+                <div class="flex-cell" role="cell">Day Trip to Martha's Vineyard</div>
+                <div class="flex-cell">12 Sep, 5p.m.</div>
+                <div class="flex-cell">US$600</div>
+            </div>
+            <div class="flex-row">
+                <div class="flex-cell">Grand Canyon West Rim and Hoover Dam Tour</div>
+                <div class="flex-cell">5 Sep, 2p.m.</div>
+                <div class="flex-cell">US$550</div>
+            </div>
+        </div>
+    </div>
+    <div class="flex-table row">
+        <div class="flex-row first" role="cell"><span class="flag-icon flag-icon-au"></span> Australia</div>
+        <div class="flex-row">Blue Mountains Tours</div>
+        <div class="flex-row">9 Sep, 2p.m.</div>
+        <div class="flex-row">US$400</div>
+    </div>
+    <div class="flex-table row">
+        <div class="flex-row first"><span class="flag-icon flag-icon-nz"></span> New Zealand</div>
+        <div class="flex-row">Milford Sound Coach & Cruise</div>
+        <div class="flex-row">12 Sep, 2p.m.</div>
+        <div class="flex-row">US$400</div>
+    </div>
+</div>
