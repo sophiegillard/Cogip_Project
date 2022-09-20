0523beaf3d6752b3986fef12e299e70083c874c9
Sophie Gillard <sophie.x.gillard@gmail.com>
2022-09-20 08:56:05 +0200

    creation of php files

---
+++ public/site/home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/css/reset.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/styles.css" rel="stylesheet" type="text/css">
+    <title>Cogip - Home</title>
+</head>
+<body>
+
+<?php 
+//require 'vendor/autoload.php';
+require '../../Resources/elements/header.php';
+?>
+
+    <main>
+
+    <h1>test</h1>
+    <?php //dump($_SERVER) ?>
+
+    </main>
+
+<?php require '../../Resources/elements/footer.php' ?>
+
+</body>
+</html>
