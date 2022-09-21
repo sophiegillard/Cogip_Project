4b8346b0f8be8105b73456006118ca94e6f44a47
Sophie Gillard <sophie.x.gillard@gmail.com>
2022-09-21 11:19:27 +0200

    functions refactored

--- public/assets/modulesJS/table/tableContent.js
+++ public/assets/modulesJS/table/tableContent.js
@@ -1,31 +1,22 @@
+import {createIndividualRow} from "./tableElement.js";
+
 /**
  * 
  * @param {DOM} position 
  * @param {array} tableHeaders 
  */
-
 export const createTableContent = (position, contentTable) =>{
     const tableDiv = document.getElementById(position);
 
-    
-
    // let arrayLength = tableHeaders.length;
     contentTable.forEach(element => {
-        const array = element;
-        console.log(element)
-        
+    
         const rowGroup = document.createElement('div');
         rowGroup.classList.add('flex__table');
         rowGroup.classList.add('table__content');
         tableDiv.appendChild(rowGroup);
 
-        element.forEach(elements => {
-        let flexTableHeader = document.createElement('div');
-        flexTableHeader.classList.add('flex__row');
-        flexTableHeader.classList.add('row__content');
-        rowGroup.append(flexTableHeader);
-        flexTableHeader.innerHTML = elements;
-        });
+        createIndividualRow(element, rowGroup);
 
         
     });
--- public/assets/modulesJS/table/tableHeader.js
+++ public/assets/modulesJS/table/tableHeader.js
@@ -1,9 +1,11 @@
+import {createIndividualRow, setDisplayTable4Columns, setDisplayTable5Columns} from "./tableElement.js";
+
+
 /**
  * 
  * @param {DOM} position 
  * @param {array} tableHeaders 
  */
-
 export const createTableHeader4 = (position, tableHeaders) =>{
     //Get the position of the main container
     const tableDiv = document.getElementById(position);
@@ -15,18 +17,9 @@ export const createTableHeader4 = (position, tableHeaders) =>{
     tableDiv.appendChild(headerDiv);
 
     //Create div for each elements
-    tableHeaders.forEach(element => {
-        let flexTableHeader = document.createElement('div');
-        flexTableHeader.classList.add('flex__row');
-        flexTableHeader.classList.add('row_header');
-        headerDiv.append(flexTableHeader);
-        flexTableHeader.innerHTML = element;
-    });
+    createIndividualRow(tableHeaders, headerDiv);
     //Adapting the column size
-    let rows= document.querySelectorAll('.flex__row');
-    rows.forEach(element => {
-        element.style.width = 'calc(100% / 4)';
-    });
+    setDisplayTable4Columns()
 }
 
 export const createTableHeader5 = (position, tableHeaders) =>{
@@ -40,17 +33,8 @@ export const createTableHeader5 = (position, tableHeaders) =>{
     tableDiv.appendChild(headerDiv);
 
     //Create div for each elements
-    tableHeaders.forEach(element => {
-        let flexTableHeader = document.createElement('div');
-        flexTableHeader.classList.add('flex__row');
-        flexTableHeader.classList.add('row_header');
-        headerDiv.append(flexTableHeader);
-        flexTableHeader.innerHTML = element;
-    });
+    createIndividualRow(tableHeaders, headerDiv);
     //Adapting the column size
-    let rows= document.querySelectorAll('.flex__row');
-    rows.forEach(element => {
-        element.style.width = 'calc(100% / 5)';
-    });
+    setDisplayTable5Columns();
 }
 
