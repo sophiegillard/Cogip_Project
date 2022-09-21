f6921ee43cf1029fc43ea3117304b1047e7787bd
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2022-09-21 15:57:58 +0200

    create structure and style of showcompanies.php

--- Controllers/ShowCompaniesController.php
+++ Controllers/ShowCompaniesController.php
@@ -10,7 +10,12 @@ class ShowCompaniesController extends Controller
     * return view
     */
     public function index()
-    {
+    {   
+        // $home = new home();
+        // $companies = $home->getCompanies();
+        // $data = [
+        //     'companies' => $companies
+        // ];
         return $this->view('showCompanies',["name" => "Cogip"]);
     }
 }
--- Resources/views/showCompanies.php
+++ Resources/views/showCompanies.php
@@ -1,23 +1,22 @@
 <?php require 'elements/header.php'; ?>
-    <main>
-        <section>
-            <h1></h1>
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
+    <main class="showCompanies__main">
+        <section class="showCompanies__main__info">
+            <h1 class="showCompanies__main__info__title">Pied Pipper</h1>
+            <p class="showCompanies__main__info__name">Name : Pied Pipper</p>
+            <p class="showCompanies__main__info__tva">TVA: BE87 876 767 565</p>
+            <p class="showCompanies__main__info__country">Country: Belgium</p>
+            <p class="showCompanies__main__info__phone">Phone: 555-5434 </p>                          
         </section>
-        <hr>
-        <section>
-            <div>
-                <img src="" alt="">
-                <p></p>
-            </div>
-            <div>
-                <img src="" alt="">
-                <p></p>
+        <hr class="showCompanies__main__hr">
+        <section class="showCompanies__main__contact">
+            <h2 class="showCompanies__main__contact__title">Contact persons</h2>
+            <div class="showCompanies__main__contact__people">
+                <div class="showCompanies__main__contact__people__card">
+                    <img class="showCompanies__main__contact__people__card__img" src="assets/img/user/user1_bertram.png" alt="">
+                    <p class="showCompanies__main__contact__people__card__name">Bertram Gilfoyle</p>
+                </div>
             </div>
         </section>
-        <hr>
+        <hr class="showCompanies__main__hr">
     </main>
 <?php require 'elements/footer.php' ?>
