b8b72542a396f2dce6c327a85d0fa3b6191874a1
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2022-09-21 16:39:45 +0200

    fix conflit class name

--- Resources/views/showContact.php
+++ Resources/views/showContact.php
@@ -1,12 +1,12 @@
 <?php require 'elements/header.php'; ?>
-    <main class="showCompanies__main">
-        <section class="showCompanies__main__info">
-            <h1 class="showCompanies__main__info__title">Bertram Gilfoyle</h1>
-            <p class="showCompanies__main__info__contact">Contact : Bertram Gilfoyle  </p>
-            <p class="showCompanies__main__info__phone">Phone : 555-5434 </p>
-            <p class="showCompanies__main__info__mail">Mail : gilfoy@piedpiper.com  </p>
-            <p class="showCompanies__main__info__company">Company : Pied Pipper  </p>
+    <main class="showContact__main">
+        <section class="showContact__main__info">
+            <h1 class="showContact__main__info__title">Bertram Gilfoyle</h1>
+            <p class="showContact__main__info__contact">Contact : Bertram Gilfoyle  </p>
+            <p class="showContact__main__info__phone">Phone : 555-5434 </p>
+            <p class="showContact__main__info__mail">Mail : gilfoy@piedpiper.com  </p>
+            <p class="showContact__main__info__company">Company : Pied Pipper  </p>
         </section>
-        <img class="showCompanies__main__img" src="assets/img/user/user1_bertram.png" alt="">
+        <img class="showContact__main__img" src="assets/img/user/user1_bertram.png" alt="">
     </main>
 <?php require 'elements/footer.php' ?>
