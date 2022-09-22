7218e6a00da4a933949b3eafb862cc1c5314b095
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2022-09-22 15:47:02 +0200

    fix conflict

--- Database/dbConnection.php
+++ Database/dbConnection.php
@@ -2,8 +2,8 @@
 
 namespace App\Database;
 
-use PDO;
-use PDOException;
+use \PDO;
+use \PDOException;
 
 class dbConnection
 {
@@ -12,15 +12,14 @@ class dbConnection
     {
         $host = 'localhost';
         $dbname = 'cogip';
-        $username = 'root';
-        $password = '';
+        $username = 'moustito';
+        $password = 'Php_My_Admin007';
 
         try {
             $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo $e->getMessage();
-            die();
         }
         return $db;
     }
