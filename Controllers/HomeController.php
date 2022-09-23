255fc703f8c56c3973e8ccdeac4b19fe74cc2646
Sophie Gillard <sophie.x.gillard@gmail.com>
2022-09-23 10:06:40 +0200

    first include of datas in home page

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -22,6 +22,9 @@ class HomeController extends Controller
             "companies" => $companies,
             "contacts" => $contacts
         ];
+        echo '<pre>';
+        var_dump($data['contacts']);
+        echo '</pre>';
         return $this->view('home', $data);
     }
 
