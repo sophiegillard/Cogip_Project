8fefd39374994d5c3ddd8d5e466b77dbf2f98ddb
Julien Scourneau <scourneaujulien@outlook.com>
2022-09-23 11:22:16 +0200

    update query for contacts and companies

--- Models/companies.php
+++ Models/companies.php
@@ -10,7 +10,7 @@ class companies
     function getAllCompanies(): bool|array
     {
         $db = (new dbConnection())->connexion();
-        $query = $db->prepare('SELECT companies.id,companies.name,tva,companies.created_at,types.name AS types FROM `companies` INNER JOIN types ON companies.types_id = types.id LIMIT 10');
+        $query = $db->prepare('SELECT companies.id,companies.name,tva,country,companies.created_at,types.name AS types FROM `companies` INNER JOIN types ON companies.types_id = types.id LIMIT 10');
         $query->execute();
         $db = null;
         return $query->fetchAll(PDO::FETCH_OBJ);
