afc939c60b54d2123aee5d95e9e0170b855d5883
Julien Scourneau <scourneaujulien@outlook.com>
2022-10-03 10:09:01 +0200

    Added company id for contacts

--- Models/contacts.php
+++ Models/contacts.php
@@ -11,7 +11,7 @@ class contacts
     function getAllContacts(): bool|array
     {
         $db = (new dbConnection())->connexion();
-        $query = $db->prepare('SELECT contacts.id,contacts.name,phone,email,contacts.created_at,companies.name AS company FROM contacts INNER JOIN companies ON contacts.company_id = companies.id LIMIT 10');
+        $query = $db->prepare('SELECT contacts.id,contacts.name,phone,email,contacts.created_at,companies.name AS company, companies.id AS companyId FROM contacts INNER JOIN companies ON contacts.company_id = companies.id LIMIT 10');
         $query->execute();
         $db = null;
         return $query->fetchAll(PDO::FETCH_OBJ);
