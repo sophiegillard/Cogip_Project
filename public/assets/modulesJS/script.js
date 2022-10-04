3c0bae5600f328e5d357daa7a3eca7129524ca2f
Sophie Gillard <sophie.x.gillard@gmail.com>
2022-10-04 16:36:19 +0200

    searchbar is working

--- public/assets/modulesJS/script.js
+++ public/assets/modulesJS/script.js
@@ -1,29 +1,8 @@
 import {sortColumnOnClick} from "./filter_table/sortTableByColumn.js";
+import {searchInputs} from "./searchBar/searchBar.js";
+
 
 sortColumnOnClick();
 
-const searchBar = document.getElementById('search');
-console.log(searchBar.value);
+searchInputs();
 
-const values = document.getElementsByClassName('row__content')
-const test = document.querySelectorAll('.row__content:nth-child(1)')
-const companies = [...test].map(n => n.textContent)
-
-const filtered = [...test].filter(n => n.textContent.includes('Jouet'))
-console.log(filtered)
-
-console.log(test);
-
-
-
-
-searchBar.addEventListener('keyup', (e)=>{
-    console.log(e.target.value);
-    let i = searchBar.value;
-
-    companies.forEach(company => {
-        console.log('test ' + company + ' = ')
-        console.log('entry '+ i)
-
-    })
-})
