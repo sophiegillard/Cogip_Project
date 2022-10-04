efa36711c4d02c2b424dc0545c39386e17c33acd
Sophie Gillard <sophie.x.gillard@gmail.com>
2022-10-04 16:42:57 +0200

    merge

--- public/assets/modulesJS/script.js
+++ public/assets/modulesJS/script.js
@@ -1,8 +1,6 @@
 import {sortColumnOnClick} from "./filter_table/sortTableByColumn.js";
 import {searchInputs} from "./searchBar/searchBar.js";
 
-
-sortColumnOnClick();
-
 searchInputs();
 
+sortColumnOnClick();
