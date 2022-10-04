58e2f1e014cb81e5471bff12bdb3cf6a321a4997
Julien Scourneau <scourneaujulien@outlook.com>
2022-10-04 14:40:58 +0200

    Added function to add new invoices in dashboardInvoicesControler

--- Controllers/DashboardInvoicesController.php
+++ Controllers/DashboardInvoicesController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 use App\Core\Controller;
+use App\Models\companies;
+use App\Models\invoices;
 
 
 class DashboardInvoicesController extends Controller
@@ -11,9 +13,23 @@ class DashboardInvoicesController extends Controller
     * return view
     */
     public function index()
-
     {
-        return $this->view('dashboardInvoices', ["name" => "Cogip"]);
+        if (isset($_POST)) {
+            var_dump($_POST);
+        }
+        $companies = new companies();
+        $company = $companies->getAllCompanies();
 
+        $data = [
+            'title' => "Contacts",
+            'company' => $company
+        ];
+        return $this->view('dashboardInvoices', $data);
+
+    }
+
+    public function addInvoices($ref, $company, $dueDate)
+    {
+        (new invoices)->createInvoices($ref, $company, $dueDate);
     }
 }
