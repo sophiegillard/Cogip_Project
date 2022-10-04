c51d7f918e95e8292b8cbf6460272c96b26f0d55
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2022-10-04 17:01:39 +0200

    js- create addeventlistener for burger menu

--- public/assets/modulesJS/displayMenuPhone.js
+++ public/assets/modulesJS/displayMenuPhone.js
@@ -1,13 +1,20 @@
-let button = document.getElementsByClassName('dashboard__header__title__menu')[0];
-let back = document.getElementsByClassName('dashboard__menu__profil__return')[0];
-let menu = document.getElementsByClassName('dashboard__menu')[0];
+// let button = document.getElementsByClassName('dashboard__header__title__menu')[0];
+let burger = document.getElementById('burger');
 
-button.addEventListener("click", (event) => {
-    console.log("display");
-    menu.style.display = "grid"; 
-});
+export function displayMenuPhone() {
+    burger.addEventListener("click", (event) => {
+        if (burger.style.display == "none") {
+            console.log("display");
+            // menu.style.display = "block"; 
+        } else {
+            console.log("none");
+            // menu.style.display = "none"; 
+        }
 
-back.addEventListener("click", (event) => {
-    console.log("back");
-    menu.style.display = "none"; 
-});
+    });
+    
+    // back.addEventListener("click", (event) => {
+    //     console.log("back");
+    //     menu.style.display = "none"; 
+    // });
+}
--- public/assets/modulesJS/script.js
+++ public/assets/modulesJS/script.js
@@ -1,5 +1,8 @@
 import {sortColumnOnClick} from "./filter_table/sortTableByColumn.js";
 import {searchInputs} from "./searchBar/searchBar.js";
+import { displayMenuPhone } from "./displayMenuPhone.js";
+
+displayMenuPhone()
 
 searchInputs();
 
