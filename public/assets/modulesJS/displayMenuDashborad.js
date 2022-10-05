40dcbbc4456f2a26f1a295f1cbde86e3fe751157
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2022-10-05 14:16:32 +0200

    fix menu phone dashboard --workinptrogress

--- public/assets/modulesJS/displayMenuDashborad.js
+++ public/assets/modulesJS/displayMenuDashborad.js
@@ -1,13 +1,13 @@
-let button = document.getElementsByClassName('dashboard__header__title__menu')[0];
-let back = document.getElementsByClassName('dashboard__menu__profil__return')[0];
-let menu = document.getElementsByClassName('dashboard__menu')[0];
+let button = document.getElementsByClassName("dashboard__header__title__menu")[0];
+let back = document.getElementsByClassName("dashboard__menu__profil__return")[0];
+let menu = document.getElementsByClassName("dashboard__menu")[0];
 
-button.addEventListener("click", (event) => {
+  button.addEventListener("click", (event) => {
     console.log("display");
-    menu.style.display = "grid"; 
-});
+    menu.style.display = "grid";
+  });
 
-back.addEventListener("click", (event) => {
+  back.addEventListener("click", (event) => {
     console.log("back");
-    menu.style.display = "none"; 
-});
+    menu.style.display = "none";
+  });
