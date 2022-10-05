1f51fb40eadeb53e0006c2fed7945b6744a822d8
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2022-10-05 16:34:35 +0200

    create new menu phone dashboard --workinprogress

--- public/assets/modulesJS/displayMenuPhone.js
+++ public/assets/modulesJS/displayMenuPhone.js
@@ -17,14 +17,3 @@ export function toggleMenu() {
 }
 
 hamburger.addEventListener("click", toggleMenu);
-
-
-if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
-  } else {
-    menu.classList.add("showMenu");
-    closeIcon.style.display = "block";
-    menuIcon.style.display = "none";
-  }
