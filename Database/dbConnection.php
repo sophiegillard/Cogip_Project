7b46d20f26cf877ea7a06ac14a106c71328f0e77
Sophie Gillard <sophie.x.gillard@gmail.com>
2022-10-06 09:59:03 +0200

    merge

--- Database/dbConnection.php
+++ Database/dbConnection.php
@@ -12,8 +12,8 @@ class dbConnection
     {
         $host = 'localhost';
         $dbname = 'cogip';
-        $username = 'moustito';
-        $password = 'root';
+        $username = 'sophro';
+        $password = '123';
 
         try {
             $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
