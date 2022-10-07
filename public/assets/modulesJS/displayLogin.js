73b5f2cec733241be56133a9b2142f9abbd513a2
moustito <moussiaux.titouan@gmail.com>
2022-10-07 11:25:47 +0200

    give hover, clear and link to login menu

--- public/assets/modulesJS/displayLogin.js
+++ public/assets/modulesJS/displayLogin.js
@@ -1,14 +1,37 @@
 let loginBtn = document.getElementById('login');
 let loginModal = document.getElementsByClassName('login')[0];
+let loginInput = document.getElementsByClassName('login__form__input');
 
 export function displayLogin() {
     loginBtn.addEventListener("click", (event)=>{
         if (loginModal.style.display == "block") {
-            console.log("none");
-        loginModal.style.display = "none";
+        
+            loginModal.style.display = "none";
+
+            for (let input of loginInput) {
+                input.value="";
+            }
+            
+            loginBtn.style.color = "#000000";
+            loginBtn.style.borderBottom = "0";
+            loginBtn.style.borderTop = "0";
+
+
+
         } else {
             console.log("click");
-        loginModal.style.display = "block";
+            loginModal.style.display = "block";
+            
+            loginBtn.style.color = "#FFFFFF";
+            loginBtn.style.borderBottom = "2px solid #ffffff";
+            loginBtn.style.borderTop = "2px solid #ffffff";
         }
     })
+}
+
+export function hoverActived() {
+    if (loginModal.style.display == "block") {
+        loginBtn.style.color = "white";
+        console.log("hello");
+    }
 }
--- public/assets/modulesJS/script.js
+++ public/assets/modulesJS/script.js
@@ -2,8 +2,10 @@ import {sortColumnOnClick} from "./filter_table/sortTableByColumn.js";
 import {searchInputs} from "./searchBar/searchBar.js";
 import { toggleMenu } from "./displayMenuPhone.js";
 import { displayLogin } from "./displayLogin.js";
+import { hoverActived } from "./displayLogin.js";
 
 displayLogin();
+hoverActived();
 
 toggleMenu();
 
